3954a7e27b6bf0ca51f7ba0152266dd7b4a3f30b
AlexAR <moltron_nano@hotmail.com>
2023-04-17 14:34:52 -0600

    feat: add rate limiter and circuit breaker for orders exeternal services

--- CloudGateway/src/main/java/com/tudux/CloudGateway/CloudGatewayApplication.java
+++ CloudGateway/src/main/java/com/tudux/CloudGateway/CloudGatewayApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
@@ -16,6 +18,11 @@ public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
+	}
+
+	@Bean
+	KeyResolver userKeyResolver() {
+		return exchange -> Mono.just("userKey");
 	}
 
 	@Bean
--- OrderService/src/main/java/com/tudux/OrderService/external/client/PaymentService.java
+++ OrderService/src/main/java/com/tudux/OrderService/external/client/PaymentService.java
@@ -1,14 +1,24 @@
 package com.tudux.OrderService.external.client;
 
+import com.tudux.OrderService.exception.CustomException;
 import com.tudux.OrderService.external.client.request.PaymentRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PAYMENT-SERVICE/payment")
 public interface PaymentService {
     @PostMapping
     public ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest) ;
+
+    default void fallback(Exception e) {
+        throw new CustomException("Payment Service is not available",
+                "UNAVAILABLE",
+                500
+        );
+    }
 }
