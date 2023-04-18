cae9d5f5f7b32a05fb7cf57d04bb0f98d3198cbd
AlexAR <moltron_nano@hotmail.com>
2023-04-18 16:04:02 -0600

    feat: add initial security configuration

---
+++ CloudGateway/src/main/java/com/tudux/CloudGateway/security/OktaAuth2WebSecurity.java
@@ -0,0 +1,25 @@
+package com.tudux.CloudGateway.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class OktaAuth2WebSecurity {
+
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .oauth2Login()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        return http.build();
+    }
+}
