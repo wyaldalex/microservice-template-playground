3e6bb15ac8378fc5de06cbc90ead833dcac06fab
AlexAR <moltron_nano@hotmail.com>
2023-04-19 13:48:37 -0600

    feat: add security to the microservices

--- OrderService/src/main/java/com/tudux/OrderService/controller/OrderController.java
+++ OrderService/src/main/java/com/tudux/OrderService/controller/OrderController.java
@@ -8,6 +8,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,6 +19,7 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @PreAuthorize("hasAuthority('Customer')")
     @PostMapping("/placeOrder")
     public ResponseEntity<Long> placeOrder(@RequestBody OrderRequest orderRequest) {
         long orderId = orderService.placeOrder(orderRequest);
@@ -25,6 +27,7 @@ public class OrderController {
         return new ResponseEntity<>(orderId, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAuthority('Customer') || hasAuthority('Admin')")
     @GetMapping("/{orderId}")
     public ResponseEntity<OrderResponse> getOrderDetails(@PathVariable("orderId") long orderId) {
         OrderResponse orderResponse =
